a952ae318af8db626a897feacc41ef22706ad79f
ANANT OM <m3@MacBook-Air.local>
2025-08-17 16:35:43 +0400

    Fix: Use correct path for public folder images

--- src/App.js
+++ src/App.js
@@ -2,33 +2,34 @@ import React from 'react';
 import './App.css'; // <-- THIS IS THE CRITICAL MISSING LINE. ADD IT.
 
 function App() {
-  return (
-    <div className="App">
+  // Inside your App() function in src/App.js
 
-      {/* Use a <header> element with the "app-header" class */}
-      <header className="app-header">
-        
-        {/* A div to group the logos together */}
-        <div className="header-logos">
-          <img src="/jindal_steel_logo.png" className="logo" alt="Jindal Steel Logo" />
-          <img src="/sohar_steel_logo.png" className="logo" alt="Sohar Steel Logo" />
-        </div>
-        
-        {/* The navigation links */}
+return (
+  <div className="App">
+    <header className="app-header">
+      
+      {/* Group the first logo and nav links together on the left */}
+      <div className="header-left">
+        <img src="/jindal_steel_logo.png" className="logo" alt="Jindal Steel Logo" />
         <nav>
           <a href="/">Home</a>
           <a href="/submit">Submit Suggestion</a>
           <a href="/my-suggestions">My Suggestions</a>
           <a href="/admin">Admin</a>
         </nav>
-      </header>
+      </div>
+      
+      {/* The second logo will be pushed to the far right by itself */}
+      <img src="/sohar_steel_logo.png" className="logo" alt="Sohar Steel Logo" />
 
-      <main>
-        {/* Your other page content will go here */}
-      </main>
+    </header>
 
-    </div>
-  );
+    <main>
+      {/* Page content will go here */}
+    </main>
+
+  </div>
+);
 }
 
 export default App;
